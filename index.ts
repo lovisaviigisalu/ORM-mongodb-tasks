12e629ccb17d1f77b65c4b2f52f868ccb5de8bd0
Lovisa Viigisalu <l.viigisalu@gmail.com>
2024-06-17 10:08:43 +0300

    "mongo-articles-authors-comments+mongo veebipood

--- index.ts
+++ index.ts
@@ -4,6 +4,23 @@ import bodyParser from "body-parser";
 import stringsController from "./controllers/strings"
 import product from "./controllers/products"
 import productlist from "./controllers/productlist"
+import articleController from "./controllers/articleController";
+import commentController from "./controllers/commentController";
+import authorController from "./controllers/authorController";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://lviigisalu:gF1bkW5XH2DKg2Hl@tsorm.m0lcexp.mongodb.net/");
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
+
 
 const app: Express = express();
 
@@ -17,9 +34,14 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 
+
+
+app.use('/', commentController);
 app.use('/', productlist)
 app.use('/', product)
 app.use('/', stringsController)
+app.use('/', articleController)
+app.use('/', authorController)
 app.listen(3001,() => {
   console.log(`[server]: Server is running at http://localhost:3001`);
 });
